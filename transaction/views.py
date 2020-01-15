17f3199c71d015eeca45028812d889cdf021a46d
nabin <naween321@gmail.com>
2020-01-16 01:38:42 +0545

    removes pass

--- transaction/views.py
+++ transaction/views.py
@@ -7,7 +7,6 @@ from transaction.models import Transaction
 def book_request(request, **kwargs):
     owner_id = Book.objects.get(id=kwargs['book_id']).user_id
     Transaction.objects.create(book_id=kwargs['book_id'], requested_by_id=kwargs['user_id'], requested_to_id=owner_id)
-    pass
     return redirect('home')
 
 
